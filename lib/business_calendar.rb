6cbcb53857168e5adfbf505caa95823cf123c1a1
Matt Larraz <mlarraz@enova.com>
2016-01-20 13:12:26 -0600

    Fix private class methods not actually being private

--- lib/business_calendar.rb
+++ lib/business_calendar.rb
@@ -1,25 +1,27 @@
 module BusinessCalendar
   CountryNotSupported = Class.new(StandardError)
+  class << self
+    def for(country)
+      Calendar.new(holiday_determiner(country))
+    end
 
-  def self.for(country)
-    Calendar.new(holiday_determiner(country))
-  end
+    private
 
-  private
-  def self.holiday_determiner(country)
-    cfg = config(country) or raise CountryNotSupported.new(country.inspect)
-    @holiday_procs ||= {}
-    @holiday_procs[country] ||= HolidayDeterminer.new(
-       cfg["regions"], 
-       cfg["holiday_names"],
-       :additions       => (cfg["additions"] || []).map { |s| Date.parse s },
-       :removals        => (cfg["removals"]  || []).map { |s| Date.parse s },
-       :additions_only  => cfg['additions_only'] )
-  end
+    def holiday_determiner(country)
+      cfg = config(country) or raise CountryNotSupported.new(country.inspect)
+      @holiday_procs ||= {}
+      @holiday_procs[country] ||= HolidayDeterminer.new(
+         cfg["regions"],
+         cfg["holiday_names"],
+         :additions       => (cfg["additions"] || []).map { |s| Date.parse s },
+         :removals        => (cfg["removals"]  || []).map { |s| Date.parse s },
+         :additions_only  => cfg['additions_only'] )
+    end
 
-  def self.config(country)
-    @config ||= YAML.load_file(File.join(File.dirname(File.expand_path(__FILE__)), '../data/holidays.yml'))
-    @config[country.to_s]
+    def config(country)
+      @config ||= YAML.load_file(File.join(File.dirname(File.expand_path(__FILE__)), '../data/holidays.yml'))
+      @config[country.to_s]
+    end
   end
 end
 
