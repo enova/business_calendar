6c05678102d05f213fcde330d4a76589e46fb074
pjapkar <pjapkar@enova.com>
2017-12-01 09:53:42 -0600

    tick version

--- spec/acceptance_br_spec.rb
+++ spec/acceptance_br_spec.rb
@@ -2,36 +2,64 @@ require 'spec_helper'
 
 describe "BR bank holidays" do
   %w(
-      2014-01-01
-      2014-04-21
-      2014-05-01
-      2014-09-07
-      2014-10-12
-      2014-11-02
-      2014-11-15
-      2014-12-25
-      2015-01-01
-      2015-04-21
-      2015-05-01
-      2015-09-07
-      2015-10-12
-      2015-11-02
-      2015-11-15
-      2015-12-25
+    2017-01-01
+    2017-01-25
+    2017-02-27
+    2017-02-28
+    2017-04-14
+    2017-04-21
+    2017-05-01
+    2017-06-15
+    2017-09-07
+    2017-10-12
+    2017-11-02
+    2017-11-15
+    2017-11-20
+    2017-12-25
+    2018-01-01
+    2018-01-25
+    2018-02-12
+    2018-02-13
+    2018-03-30
+    2018-04-21
+    2018-05-01
+    2018-05-31
+    2018-09-07
+    2018-10-12
+    2018-11-02
+    2018-11-15
+    2018-11-20
+    2018-12-25
     ).map { |x| Date.parse x }.each do |expected_holiday|
     it "treats #{expected_holiday} as a holiday" do
       expect(BusinessCalendar.for(:BR).is_holiday?(expected_holiday)).to be true
     end
   end
 
-  # Carnival days
   %w(
-      2014-02-28
-      2014-03-03
-      2014-03-04
+      2018-12-26
+      2017-01-02
     ).map { |x| Date.parse x }.each do |date|
     it "treats #{date} as not a holiday" do
       expect(BusinessCalendar.for(:BR).is_holiday?(date)).to be false
     end
+  end
+
+  # Saturday and Sunday
+  %w(
+      2018-11-25
+      2017-11-26
+    ).map { |x| Date.parse x }.each do |date|
+    it "treats #{date} as not a business day" do
+      expect(BusinessCalendar.for(:BR).is_business_day?(date)).to be false
+    end
+  end
+
+  it "calculates previous business day correctly" do
+    expect(BusinessCalendar.for(:BR).preceding_business_day(Date.parse('2018-11-19'))).to eq (Date.parse('2018-11-16'))
+  end
+
+  it "calculates next business day correctly" do
+    expect(BusinessCalendar.for(:BR).following_business_day(Date.parse('2018-11-19'))).to eq (Date.parse('2018-11-21'))
   end
 end
